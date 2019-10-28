baf42264f40ed1fa9fff1410619b544cd36cb2ba
MURATA Hana <21716095@jwu.ac.jp>
2019-10-28 16:28:46 +0900

    parser2

--- parser.py
+++ parser.py
@@ -1,19 +1,13 @@
 from exp import Val, Add
 
 def parse(s: str):
-    num = int(s)
-    return Val(num)
+    pos = s. find('+') #'+'記号を探す
+    if pos == -1:
+        return Val(int(s))
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
 
-e = parse("123")
-print(e)
-
-s = "123+456"
-pos = s. find('+') #'+'記号を探す
-print('pos', pos)
-
-s1 = s[0:pos]
-s2 = s[pos+1:]
-print(s, s1, s2) #'+'記号で分割
-
-
-
+e = parse("1+2+3")
+print(e, e.eval())
