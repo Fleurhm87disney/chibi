9833d0824ab1070cd5f49c5cb86aa923fd0683b8
MURATA Hana <21716095@jwu.ac.jp>
2019-11-25 16:15:19 +0900

    icalc''

--- icalc.py
+++ icalc.py
@@ -11,12 +11,20 @@ def calc(t):
     if t == 'Int':
         return int(str(t))
     if t == 'Add':
+        return calc(t[0]) + calc(t[1])
+    if t == 'Mul':
         return calc(t[0]) * calc(t[1])
     print(f'TODO {t.tag}')
     return 0 
 
-t = parser('1+2*3')
-print(repr(t))
+#t = parser('1+2*3+4*5')
+#print(repr(t))
+#print(calc(t))
 
-t = parser('@2')
-print(repr(t))
+def main():
+    s = input('$ ')
+    t = parser(s)
+    print(calc(t))
+
+if __name__ == '__main__':
+    main()
