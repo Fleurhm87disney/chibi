f2b3aa4264793fb2de49f6ac0d163f7bae89e077
MURATA Hana <21716095@jwu.ac.jp>
2019-11-25 15:58:03 +0900

    icalc

---
+++ icalc.py
@@ -0,0 +1,22 @@
+import pegpy
+
+peg = pegpy.grammar('''
+Expression = product (^{ Value: str #Add })*
+Product = Value (^{ '*' Value #Mul})*
+Value = { [0-9]+ #Int }
+''')
+parser =  pegpy.generate(peg)
+
+def calc(t):
+    if t == 'Int':
+        return int(str(t))
+    if t == 'Add':
+        return calc(t[0]) * calc(t[1])
+    print(f'TODO {t.tag}')
+    return 0 
+
+t = parser('1+2*3')
+print(repr(t))
+
+t = parser('@2')
+print(repr(t))
